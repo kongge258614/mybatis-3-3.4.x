f8b5fdfb8e296e394ccf9f843e2cc607558b9cd9
liliang <dzliliang@126.com>
2019-11-22 11:00:25 +0800

    commit

---
+++ src/main/java/org/apache/ibatis/liliang/java/com/lkker/Test.java
@@ -0,0 +1,25 @@
+package org.apache.ibatis.liliang.java.com.lkker;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @Author liliang
+ * @Date 2019/11/22 10:52
+ * @Description
+ **/
+public class Test {
+
+    public static void main(String[] args) throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+    }
+}
---
+++ src/main/java/org/apache/ibatis/liliang/java/com/lkker/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package org.apache.ibatis.liliang.java.com.lkker.mapper;
+
+/**
+ * @Author liliang
+ * @Date 2019/11/22 10:51
+ * @Description
+ **/
+public interface UserMapper {
+
+}

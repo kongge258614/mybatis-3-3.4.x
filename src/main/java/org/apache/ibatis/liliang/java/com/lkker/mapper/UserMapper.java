93168050c7af16392e21338e5f8d1830d6f7ae3b
liliang <liliang@laiyuezs.com>
2021-01-22 11:02:06 +0800

    commit

--- src/main/java/org/apache/ibatis/liliang/java/com/lkker/mapper/UserMapper.java
+++ src/main/java/org/apache/ibatis/liliang/java/com/lkker/mapper/UserMapper.java
@@ -2,6 +2,8 @@ package org.apache.ibatis.liliang.java.com.lkker.mapper;
 
 import org.apache.ibatis.liliang.java.com.lkker.entity.User;
 
+import java.util.List;
+
 /**
  * @Author liliang
  * @Date 2019/11/22 10:51
@@ -10,5 +12,7 @@ import org.apache.ibatis.liliang.java.com.lkker.entity.User;
 public interface UserMapper {
 
     User selectUserById(int id);
+
+    List<User> selectUserByName(String userName);
 
 }
--- src/main/java/org/apache/ibatis/liliang/java/com/lkker/plugin/CustomPlugin.java
+++ src/main/java/org/apache/ibatis/liliang/java/com/lkker/plugin/CustomPlugin.java
@@ -1,12 +1,24 @@
 package org.apache.ibatis.liliang.java.com.lkker.plugin;
 
 import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.mapping.ParameterMapping;
+import org.apache.ibatis.mapping.ParameterMode;
 import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.RowBounds;
+import org.apache.ibatis.type.TypeHandlerRegistry;
+import org.slf4j.LoggerFactory;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 import java.util.Properties;
+import java.util.regex.Matcher;
 
 /**
  * @Author liliang
@@ -16,14 +28,27 @@ import java.util.Properties;
 @Intercepts(value = {@Signature(type = Executor.class,method = "query",args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class})})
 public class CustomPlugin implements Interceptor {
 
+    private static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
     // 这里是每次执行操作的时候，都会进行这个拦截器的方法内
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
-
-        System.out.println("用户自定义插件....................");
-        String name = invocation.getMethod().getName();
-        System.out.println("name:"+name);
-        return invocation.proceed();
+        MappedStatement mappedStatement = (MappedStatement) invocation.getArgs()[0];
+        Object parameterObject = null;
+        if (invocation.getArgs().length>1){
+            parameterObject = invocation.getArgs()[1];
+        }
+        long start = System.currentTimeMillis();
+        Object result = invocation.proceed();
+        String statementId = mappedStatement.getId();
+        BoundSql boundSql = mappedStatement.getBoundSql(parameterObject);
+        Configuration configuration = mappedStatement.getConfiguration();
+        String sql = getSql(boundSql, parameterObject, configuration);
+        long end = System.currentTimeMillis();
+        long time =  end - start;
+        System.out.println("执行sql耗时:" + time + "ms" + "- id:" +statementId );
+        System.out.println("- SQL:" + sql);
+        return result;
     }
 
     // 主要是为了把这个拦截器生成一个代理放到拦截器链中
@@ -37,5 +62,50 @@ public class CustomPlugin implements Interceptor {
     @Override
     public void setProperties(Properties properties) {
 
+    }
+
+    private String getSql(BoundSql boundSql, Object parameterObject, Configuration configuration){
+        String sql = boundSql.getSql().replaceAll("[\\s]+"," ");
+        List<ParameterMapping> parameterMappings = boundSql.getParameterMappings();
+        TypeHandlerRegistry typeHandlerRegistry = configuration.getTypeHandlerRegistry();
+        if (parameterMappings != null){
+            for (int i = 0;i<parameterMappings.size();i++){
+                ParameterMapping parameterMapping = parameterMappings.get(i);
+                if (parameterMapping.getMode() != ParameterMode.OUT){
+                    Object value;
+                    String propertyName = parameterMapping.getProperty();
+                    if (boundSql.hasAdditionalParameter(propertyName)){
+                        value = boundSql.getAdditionalParameter(propertyName);
+
+                    }else if (parameterObject == null){
+                        value = null;
+                    }else if (typeHandlerRegistry.hasTypeHandler(parameterObject.getClass())){
+                        value = parameterObject;
+                    }else {
+                        MetaObject metaObject = configuration.newMetaObject(parameterObject);
+                        value = metaObject.getValue(propertyName);
+                    }
+                    sql = replacePlaceHolder(sql,value);
+                }
+            }
+        }
+        return sql;
+    }
+
+    private String replacePlaceHolder(String sql,Object propertyValue){
+        String result = null;
+        if (propertyValue != null){
+            if (propertyValue instanceof String){
+                result = "'" + propertyValue + "'";
+            }else if (propertyValue instanceof Date){
+                result = "'" + DATE_FORMAT.format(propertyValue) + "'";
+            }else {
+                result = propertyValue.toString();
+            }
+
+        }else {
+            result = "null";
+        }
+        return sql.replaceFirst("\\?", Matcher.quoteReplacement(result));
     }
 }
